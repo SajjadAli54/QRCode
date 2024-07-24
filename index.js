d7ac010adaff709296274c22d22abc3d14ed0830
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2024-07-25 01:40:56 +0500

    Generate POST API for generating the qrcode image

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const { createImage, saveImage } = require("./src/persistance");
 
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
 app.get("/generateQR", async (req, res) => {
@@ -16,6 +17,23 @@ app.get("/generateQR", async (req, res) => {
     saveImage(url, imageData);
 
     res.send(`<img src="${qrCodeImage}" alt="QR Code"/>`);
+  } catch (err) {
+    console.error("Error generating QR code:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/generateQR", async (req, res) => {
+  try {
+    const { url } = req.body;
+    console.log("URL", url);
+
+    const qrCodeImage = await createImage(url);
+    const imageData = qrCodeImage.split(",")[1];
+
+    saveImage(url, imageData);
+
+    res.json({ imageData });
   } catch (err) {
     console.error("Error generating QR code:", err);
     res.status(500).send("Internal Server Error");
