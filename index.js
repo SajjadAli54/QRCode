f10370f738476ad10120d37d88ffec6811b6d98c
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2024-07-25 01:24:25 +0500

    Refactor code to have in src dir

--- index.js
+++ index.js
@@ -1,67 +1,20 @@
 // app.js
 
-const QRCode = require("qrcode");
-const uuid = require("uuid");
-
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const QRCode = require("qrcode");
+const { createImage, saveImage } = require("./src/persistance");
 
 const app = express();
 const PORT = 3000;
 
-const data_path = path.join(__dirname, "data", "data.json");
-
-let data = [];
-try {
-  data = require(data_path);
-} catch (err) {
-  console.error(err);
-}
-
-const qrCodeOptions = {
-  color: {
-    dark: "#00F", // Blue dots
-    light: "#0000", // Transparent background
-  },
-};
-
-const saveImage = (imageData, imageUrl) => {
-  const labelId = uuid.v4();
-  const qrCodeData = `${imageUrl}${labelId}`;
-  const qrCodeImagePath = path.join(
-    __dirname,
-    "images",
-    `qrcode_${labelId}.png`
-  );
-
-  console.log("PATH", qrCodeImagePath);
-
-  QRCode.toFile(qrCodeImagePath, qrCodeData, qrCodeOptions, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    const newData = {
-      id: labelId,
-      src: imageUrl,
-      imageUrl: qrCodeData,
-      imagePath: qrCodeImagePath,
-    };
-    data.push(newData);
-    fs.writeFileSync(data_path, JSON.stringify(data, null, 2));
-  });
-};
-
 app.get("/generateQR", async (req, res) => {
   try {
     const url = req.query.url || "https://example.com";
-    const qrCodeImage = await QRCode.toDataURL(url, qrCodeOptions);
 
+    const qrCodeImage = await createImage(url);
     const imageData = qrCodeImage.split(",")[1];
 
-    saveImage(imageData, url);
+    saveImage(url, imageData);
 
     res.send(`<img src="${qrCodeImage}" alt="QR Code"/>`);
   } catch (err) {
---
+++ src/persistance.js
@@ -0,0 +1,62 @@
+const QRCode = require("qrcode");
+const uuid = require("uuid");
+const path = require("path");
+const fs = require("fs");
+
+const data_path = path.join(__dirname, "..", "data", "data.json");
+
+let data = [];
+try {
+  data = require(data_path);
+} catch (err) {
+  console.error(err);
+}
+
+const qrCodeOptions = {
+  color: {
+    dark: "#00F", // Blue dots
+    light: "#0000", // Transparent background
+  },
+};
+
+const createImage = async (url) => {
+  return await QRCode.toDataURL(url, qrCodeOptions);
+};
+
+const saveImage = async (url, imageData) => {
+  const { labelId, qrCodeData, qrCodeImagePath } = getMetaData(url);
+
+  QRCode.toFile(qrCodeImagePath, qrCodeData, qrCodeOptions, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    addInfo(labelId, url, qrCodeData, qrCodeImagePath);
+  });
+};
+
+const addInfo = (labelId, imageUrl, qrCodeData, qrCodeImagePath) => {
+  const newData = {
+    id: labelId,
+    src: imageUrl,
+    imageUrl: qrCodeData,
+    imagePath: qrCodeImagePath,
+  };
+  data.push(newData);
+  fs.writeFileSync(data_path, JSON.stringify(data, null, 2));
+};
+
+const getMetaData = (imageUrl) => {
+  const labelId = uuid.v4();
+  const qrCodeData = `${imageUrl}/${labelId}`;
+  const qrCodeImagePath = path.join(
+    __dirname,
+    "..",
+    "images",
+    `qrcode_${labelId}.png`
+  );
+
+  return { labelId, qrCodeData, qrCodeImagePath };
+};
+module.exports = { saveImage, createImage };
